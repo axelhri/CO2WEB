fa00321bde8989fcdd79037bce5ef76dadf5ed74
Kliver59 <christopher59880@hotmail.fr>
2024-05-16 13:11:47 +0200

    ecowebapropos

---
+++ javascript/apropos.js
@@ -0,0 +1,7 @@
+const slideapropos2 = ["Groupe146.svg", "Groupe40.svg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    document.getElementById("slideapropos2").src = "images/" + slideapropos[0];
+}
+
