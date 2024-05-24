3570ea0aea3249d15327610ccba39b4f1578c307
Camus59 <sebp59220@gmail.com>
2024-05-24 15:52:17 +0200

    d

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burgermenu');
     const burgerList = document.querySelector('.burgerlist');
+    const slideButtons = document.querySelectorAll('.slide-button');
 
     burgerMenu.addEventListener('click', function() {
         burgerList.classList.toggle('active');
         burgerMenu.classList.toggle('active');
         document.body.classList.toggle('no-scroll');
+        
+        slideButtons.forEach(button => {
+            button.classList.toggle('hide-slide-buttons');
+        });
     });
 });
